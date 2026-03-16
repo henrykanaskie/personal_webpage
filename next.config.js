656f75cd5fa435cb7631e460e296e22f1336f38d
henrykanaskie <howenkan13@gmail.com>
2026-03-16 09:10:24 -0700

    build hotfix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingExcludes: {
-      "/photography/[category]": ["./public/photography/**/*"],
-    },
+  outputFileTracingExcludes: {
+    "/photography/[category]": ["./public/photography/**/*"],
   },
 };
 
