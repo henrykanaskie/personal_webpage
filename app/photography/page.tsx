7a8209c275f04da0c4ad152a786e31b2dcbbcc88
henrykanaskie <howenkan13@gmail.com>
2026-03-15 22:51:48 -0700

    resized images

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -1,6 +1,8 @@
 import { buildSections } from "./getPhotos";
 import PhotoGalleryClient from "./PhotoGalleryClient";
 
+export const dynamic = "force-static";
+
 export default async function PhotographyPage() {
   const sections = await buildSections();
   return <PhotoGalleryClient sections={sections} />;
